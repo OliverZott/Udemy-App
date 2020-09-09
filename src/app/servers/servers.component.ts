6ab991ed41236cc078b0cd83bfe238a094e505f6
OliverZott <zott_oliver@web.de>
2020-09-09 08:37:17 +0200

    Added - Template & Styles testing

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+    selector: 'app-servers',
+    // template: '<app-server></app-server>',
+    /* template: `
+        <app-server>
+            blub
+        </app-server>`,
+    */
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
